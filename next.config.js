2724ad3ba370852daa64a145015b362c3cbe3ec1
dubaiwebcity <dubaiwebcity@gmail.com>
2025-10-29 14:45:15 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
-
-  // Azure deployment ke liye standalone output
-  output: 'standalone',
-
-  // Agar tum images manually host kar rahe ho (Next Image Optimization band karne ke liye)
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
