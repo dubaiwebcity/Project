3dca253172f9df9f20cad510b8afdec7760bbde1
dubaiwebcity <dubaiwebcity@gmail.com>
2025-11-11 22:39:25 +0500

    Update page.client.tsx

--- src/app/en/bnoon-riyadh/page.client.tsx
+++ src/app/en/bnoon-riyadh/page.client.tsx
@@ -5,7 +5,7 @@ export default function ClientBnoonRiyadhPage() {
   return (
     <>
       <Navbar />
-      <PageBanner bgImage="/images/clinics-banner.avif" />
+      <PageBanner bgImage="/images/clinics-banner.jpg" />
        <Riyadharea />
 
     </>
