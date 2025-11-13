3b0992a0890d697e4743adfbfd80220ffa3561f0
dubaiwebcity <dubaiwebcity@gmail.com>
2025-11-13 12:30:38 +0500

    Update DrAsimAlWohaibi.tsx

--- src/components/Common/DrAsimAlWohaibi.tsx
+++ src/components/Common/DrAsimAlWohaibi.tsx
@@ -125,7 +125,7 @@ const DrAsimAlWohaibi = () => {
 
 
 
-          <div className="col-xl-5 col-md-12 d-flex flex-column justify-content-center text-center">
+          <div className="col-xl-5 col-md-12 d-flex flex-column justify-content-center text-center image-column">
             <img
               className="doctors-overview-image"
               src={imageRiyadh}
