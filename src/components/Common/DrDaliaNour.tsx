a792e05f9eb72ca28287e83fec31c72453807740
dubaiwebcity <dubaiwebcity@gmail.com>
2025-11-13 12:45:07 +0500

    Update DrDaliaNour.tsx

--- src/components/Common/DrDaliaNour.tsx
+++ src/components/Common/DrDaliaNour.tsx
@@ -128,7 +128,7 @@ const DrDaliaNour = () => {
 
 
 
-          <div className="col-xl-5 col-md-12 d-flex flex-column justify-content-center text-center">
+          <div className="col-xl-5 col-md-12 d-flex flex-column justify-content-center text-center image-column">
             <img
               className="doctors-overview-image"
               src={imageRiyadh}
